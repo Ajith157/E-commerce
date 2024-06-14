1546c45e972ce028d092020562d21ac0026a3df6
Ajith P R <ajithpr9999@gmail.com>
2024-06-14 15:10:46 +0530

    Your commit message

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/userRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
-const ConnectMongodbSession = require('connect-mongodb-session');
+const ConnectMongodbSession = require('connect-mongodb-session')(session);
 const multer = require('multer');
 const path = require('path');  
 const fs = require('fs');     
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public/backend')));
 
 const corsOptions = {
-  origin: true, // Allow requests from any origin
+  origin: 'https://janki-design.onrender.com',
   credentials: true, // Allow credentials (cookies) to be sent
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -37,9 +37,9 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.set('trust proxy', 1);
 
-const store = new (ConnectMongodbSession(session))({
+const store = new ConnectMongodbSession({
   uri: process.env.MONGODB_URL,
-  collection: "session",
+  collection: "sessions",
   databaseName: process.env.DATABASE_NAME
 });
 
@@ -53,8 +53,8 @@ app.use(session({
   resave: false,
   store: store,
   cookie: {
-    maxAge: 1000 * 60 * 24 * 10,
-    sameSite: 'none',
+    maxAge: 1000 * 60 * 24 * 10, // 10 days
+    sameSite: 'none', // Allow cross-site cookies
     secure: process.env.NODE_ENV === 'production', // Ensure this matches your environment setup
   },
 }));
@@ -71,5 +71,5 @@ app.use('/admin', adminRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Dev server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`);
 });

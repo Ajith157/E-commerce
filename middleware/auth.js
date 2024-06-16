2b3c5716183dbf4aae47c38d9078699cfb0235aa
Ajith P R <ajithpr9999@gmail.com>
2024-06-16 19:36:46 +0530

    Your commit message

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,20 +1,30 @@
 module.exports = {
   adminAuth: (req, res, next) => {
-    if (req.session.admin) {
-      next();
-    } else {
-      res.status(401).json({ error: 'Unauthorized' });
+    try {
+      if (req.session.admin) {
+        next();
+      } else {
+        res.status(401).json({ error: 'Unauthorized' });
+      }
+    } catch (error) {
+      console.error('Error in adminAuth middleware:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
   },
 
   userAuth: (req, res, next) => {
-     
-    if (req.session.user) {
-      next();
-    } else {
-      console.error('User not authenticated or missing _id');
-      res.status(401).json({ error: 'Unauthorized' });
+    try {
+      if (req.session.user) {
+        next();
+      } else {
+        console.error('User not authenticated or missing _id');
+        res.status(401).json({ error: 'Unauthorized' });
+      }
+    } catch (error) {
+      console.error('Error in userAuth middleware:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
     }
   },
 };
+
   
